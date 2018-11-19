0ddb456a95a23a71c0749b61d48c1449159a2f4a
Rodolfo Saldivar <rodolfo saldivar@TDG-MBL-CHA067.lan>
2018-11-19 14:07:01 -0600

    ready to start

---
+++ client/src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = (props) => (
+  <div>
+    Working fine
+  </div>
+);
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+	res.send({ working: 'fine' });
+});
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+app.listen(process.env.PORT || 5000);
